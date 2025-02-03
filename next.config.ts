a6a4bdd22d079085ae1fae79bd4accce1a8e8ddc
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2025-02-03 22:25:09 +0900

    chore: setup Next.js 프로젝트

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (
+    config,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    return config;
+  },
 };
 
 export default nextConfig;
