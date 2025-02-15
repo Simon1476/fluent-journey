8c6c6810b36cefaff503d6cf22aaf725244b1645
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2025-02-15 18:40:48 +0900

    feat: DeleteWordAlertDialogContent 컴포넌트 추가

---
+++ src/features/wordlists/components/DeleteWordAlertDialogContent.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import {
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { useToast } from "@/hooks/use-toast";
+import { useTransition } from "react";
+import { deleteWord } from "../server/actions/wordlists";
+
+interface Props {
+  wordId: string;
+  listId: string;
+}
+export function DeleteWordAlertDialogContent({ wordId, listId }: Props) {
+  const [isDeletePending, startDeleteTransition] = useTransition();
+  const { toast } = useToast();
+
+  return (
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+        <AlertDialogDescription>
+          This action cannot be undone. This will permanently delete this
+          product.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction
+          onClick={() => {
+            startDeleteTransition(async () => {
+              const data = await deleteWord(listId, wordId);
+              if (data.message) {
+                toast({
+                  title: data.error ? "Error" : "Success",
+                  description: data.message,
+                  variant: data.error ? "destructive" : "default",
+                });
+              }
+            });
+          }}
+          disabled={isDeletePending}
+        >
+          Delete
+        </AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  );
+}
