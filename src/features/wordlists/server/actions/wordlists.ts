bee7120addc75fda0eb7d524464b11725306143a
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2025-02-17 16:25:15 +0900

    feat: 단어장 조회 캐싱 적용 및 사용자 단어 삭제 기능 추가

--- src/features/wordlists/server/actions/wordlists.ts
+++ src/features/wordlists/server/actions/wordlists.ts
@@ -10,6 +10,7 @@ import { redirect } from "next/navigation";
 import {
   createCustomWord as createCustomWordDb,
   createUserWord as createUserWordDb,
+  deleteUserWord as deleteUserWordDb,
 } from "@/features/wordlists/server/db/wordlists";
 
 export async function createWordList(
@@ -61,5 +62,19 @@ export async function createWord(
   return {
     error: false,
     message: "단어장에 단어를 추가했습니다.",
+  };
+}
+
+export async function deleteWord(listId: string, wordId: string) {
+  const session = await auth();
+  const accountId = session?.user.id;
+  const userId = await getUserId(accountId);
+
+  const errorMessage = "단어를 삭제하지 못했습니다.";
+  const isSuccess = await deleteUserWordDb(listId, wordId, userId);
+
+  return {
+    error: !isSuccess,
+    message: isSuccess ? "성공적으로 단어를 삭제 했습니다." : errorMessage,
   };
 }
--- src/features/wordlists/server/db/wordlists.ts
+++ src/features/wordlists/server/db/wordlists.ts
@@ -4,41 +4,40 @@ import { prisma } from "@/lib/prisma";
 import { getUserId } from "@/lib/utils";
 import { wordListWordSchema } from "@/features/wordlists/schemas/wordlists";
 import { z } from "zod";
+import {
+  CACHE_TAGS,
+  revalidateDbCache,
+  dbCache,
+  getGlobalTag,
+  getUserTag,
+  getIdTag,
+} from "@/lib/cache";
 
 export async function getWordLists(accountId: string) {
   const userId = await getUserId(accountId);
 
-  const lists = await prisma.userWordList.findMany({
-    where: {
-      userId,
-    },
-    include: {
-      _count: {
-        select: { words: true },
-      },
-    },
+  const cacheFn = dbCache(getWordlistsInternal, {
+    tags: [
+      getGlobalTag(CACHE_TAGS.wordlists),
+      getUserTag(userId, CACHE_TAGS.wordlists),
+    ],
   });
-  return lists;
+
+  return cacheFn(userId);
 }
 
 export async function getWordListById(accountId: string, id: string) {
   const userId = await getUserId(accountId);
 
-  const wordList = await prisma.userWordList.findUnique({
-    where: {
-      id: id,
-    },
-    include: {
-      words: {
-        include: {
-          word: true,
-          customWord: true,
-        },
-      },
-    },
+  const cacheFn = dbCache(getWordlistsByIdInternal, {
+    tags: [
+      getGlobalTag(CACHE_TAGS.wordlists),
+      getUserTag(userId, CACHE_TAGS.wordlists),
+      getIdTag(id, CACHE_TAGS.wordlists),
+    ],
   });
 
-  return wordList;
+  return cacheFn(id);
 }
 
 export async function createCustomWord(
@@ -60,13 +59,80 @@ export async function createCustomWord(
 }
 
 export async function createUserWord(listId: string, customWordId: string) {
-  return await prisma.userWord.create({
+  const result = await prisma.userWord.create({
     data: {
-      wordListId: listId,
-      customWordId: customWordId,
+      wordList: { connect: { id: listId } },
+      customWord: { connect: { id: customWordId } },
     },
     include: {
       customWord: true,
     },
+  });
+
+  revalidateDbCache({
+    tag: CACHE_TAGS.wordlists,
+    id: listId,
+  });
+
+  return result;
+}
+
+export async function deleteUserWord(
+  listId: string,
+  wordId: string,
+  userId: string
+) {
+  // 권한 확인
+  const wordList = await getUserWordListInternal(listId, userId);
+  if (!wordList) return false;
+
+  await deleteUserWordInternal(wordId);
+
+  revalidateDbCache({
+    tag: CACHE_TAGS.wordlists,
+    id: listId,
+    userId,
+  });
+
+  return true;
+}
+
+async function getWordlistsInternal(userId: string) {
+  return await prisma.userWordList.findMany({
+    where: { userId },
+    include: {
+      _count: {
+        select: { words: true },
+      },
+    },
+  });
+}
+
+async function getWordlistsByIdInternal(id: string) {
+  return await prisma.userWordList.findUnique({
+    where: { id },
+    include: {
+      words: {
+        include: {
+          word: true,
+          customWord: true,
+        },
+      },
+    },
+  });
+}
+
+async function getUserWordListInternal(listId: string, userId: string) {
+  return prisma.userWordList.findUnique({
+    where: {
+      id: listId,
+      userId: userId,
+    },
+  });
+}
+
+async function deleteUserWordInternal(wordId: string) {
+  return prisma.userWord.delete({
+    where: { id: wordId },
   });
 }
