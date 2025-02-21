5556c1a0bc478b61cfead1e971c02e98d697d7b8
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2025-02-21 23:48:34 +0900

    feat: CACHE_TAGS에 sharedWordlists 추가

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -8,6 +8,7 @@ export type ValidTags =
 
 export const CACHE_TAGS = {
   wordlists: "wordlists",
+  sharedWordlists: "sharedWordlists",
 } as const;
 
 export function getGlobalTag(tag: keyof typeof CACHE_TAGS) {
