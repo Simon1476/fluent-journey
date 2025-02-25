7aca17c3390c3bd7fc512a64d11bc3b8ef6f4087
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2025-02-25 17:37:40 +0900

    feat: Kakao 및 Google 로그인 처리 로직 추가 및 JWT/세션 콜백 수정

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,28 +1,45 @@
-import { KakaoProfile } from "@/types/kakaoProfile";
+import { KakaoAccount, KakaoProfile } from "@/types/kakaoProfile";
+import { GoogleProfile, GoogleAccount } from "@/types/googleProfile";
 import Kakao from "next-auth/providers/kakao";
+import Google from "next-auth/providers/google";
 
 import type { NextAuthConfig } from "next-auth";
 
 export default {
-  providers: [Kakao],
+  providers: [Kakao, Google],
   callbacks: {
     async jwt({ token, account, profile }) {
-      if (account && profile) {
+      if (!account || !profile) return token;
+
+      if (account.provider === "kakao") {
+        const kakaoAccount = account as unknown as KakaoAccount;
         const kakaoProfile = profile as unknown as KakaoProfile;
-        token.accessToken = account.access_token;
-        token.refreshToken = account.refresh_token;
-        token.expiresAt = account.expires_at;
+        token.accessToken = kakaoAccount.access_token;
+        token.refreshToken = kakaoAccount.refresh_token;
+        token.expiresAt = kakaoAccount.expires_at;
         token.userId = kakaoProfile.id;
-        token.nickname = kakaoProfile.properties.nickname;
-        token.profileImage = kakaoProfile.properties.profile_image;
+        token.nickname = kakaoProfile.properties.nickname ?? "";
+        token.profileImage = kakaoProfile.properties.profile_image ?? "";
+      } else if (account.provider === "google") {
+        const googleAccount = account as unknown as GoogleAccount;
+        const googleProfile = profile as unknown as GoogleProfile;
+
+        token.accessToken = googleAccount.access_token;
+        token.refreshToken = account.refresh_token;
+        token.expiresAt = googleAccount.expires_at;
+        token.userId = googleProfile.sub;
+        token.nickname = googleProfile.name;
+        token.profileImage = googleProfile.picture;
       }
+
       return token;
     },
     async session({ session, token }) {
       session.user.id = token.userId;
       session.user.nickname = token.nickname;
       session.user.profileImage = token.profileImage;
-
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
       return session;
     },
   },
