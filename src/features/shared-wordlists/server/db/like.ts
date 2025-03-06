e6b1f1198dce4ae13f2f15e3a8b9ca5a9e5e873b
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2025-03-06 15:57:35 +0900

    feat: 좋아요 토글 서버 액션 및 DB 로직 추가

---
+++ src/features/shared-wordlists/server/db/like.ts
@@ -0,0 +1,80 @@
+import {
+  CACHE_TAGS,
+  dbCache,
+  getGlobalTag,
+  getUserTag,
+  revalidateDbCache,
+} from "@/lib/cache";
+import { prisma } from "@/lib/prisma";
+import { getUserId } from "@/lib/utils";
+
+export async function toggleLike(listId: string, userId: string) {
+  const existingLike = await prisma.like.findUnique({
+    where: {
+      userId_listId: {
+        userId: userId,
+        listId: listId,
+      },
+    },
+  });
+
+  if (existingLike) {
+    await prisma.like.delete({
+      where: {
+        id: existingLike.id,
+      },
+    });
+
+    revalidateDbCache({
+      tag: CACHE_TAGS.likes,
+      userId: userId,
+    });
+
+    return {
+      error: false,
+      message: "좋아요를 취소했습니다.",
+    };
+  } else {
+    await prisma.like.create({
+      data: {
+        id: listId,
+        userId: userId,
+        listId: listId,
+      },
+    });
+
+    revalidateDbCache({
+      tag: CACHE_TAGS.likes,
+      userId: userId,
+    });
+
+    return {
+      error: false,
+      message: "좋아요를 추가했습니다.",
+    };
+  }
+}
+
+export async function getLikes(accountId: string) {
+  const userId = await getUserId(accountId);
+  if (userId == null) return [];
+
+  const cacheFn = dbCache(getLikesInternal, {
+    tags: [
+      getGlobalTag(CACHE_TAGS.likes),
+      getUserTag(userId, CACHE_TAGS.likes),
+    ],
+  });
+
+  return cacheFn(userId);
+}
+
+async function getLikesInternal(userId: string) {
+  const likes = await prisma.like.findMany({
+    where: {
+      userId: userId,
+    },
+  });
+
+  return likes;
+}
